bb4d84c89beae20497a9c0cd29a7fe5110576b11
Otávio de Castro <otavio.vn01@hotmail.com>
2023-07-25 17:53:58 -0300

    Commit inicial

---
+++ src/main/java/com/blackberry/blackberrypizzas/BlackberryPizzasApplication.java
@@ -0,0 +1,13 @@
+package com.blackberry.blackberrypizzas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackberryPizzasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackberryPizzasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blackberry/blackberrypizzas/BlackberryPizzasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackberry.blackberrypizzas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackberryPizzasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
